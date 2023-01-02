8413dd9d89d04be6f0518080e18fecf1ea5a98bc
Ehab Heakal <ehabheakal@gmail.com>
2023-01-02 22:07:48 +0400

    adding front_end button and fuction in main

--- front_end.py
+++ front_end.py
@@ -5,3 +5,4 @@ Created on Mon Jan  2 21:26:28 2023
 @author: 20924
 """
 
+button1 = red
--- main.py
+++ main.py
@@ -4,4 +4,6 @@ Created on Mon Jan  2 21:16:23 2023
 
 @author: 20924
 """
-
+def multiply(y,x):
+    return x * y
+multiply (4,6)
