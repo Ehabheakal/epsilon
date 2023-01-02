1da201384b2903ba897c0736c852b61a137763e0
Ehab Heakal <ehabheakal@gmail.com>
2023-01-02 21:30:54 +0400

    adding front_end and calculator files

---
+++ calculator.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  2 21:26:15 2023
+
+@author: 20924
+"""
+
---
+++ front_end.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  2 21:26:28 2023
+
+@author: 20924
+"""
+
